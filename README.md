a2d1b4a30b7110118248177ed7eab439afd6c804
gunnsmith <74391528+gunnsmith@users.noreply.github.com>
2025-08-10 22:07:34 -0400

    fix

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Jira-Automation
- 
+
+Python script for automating the creation of tickets in Jira with "boilerplate text".
+(including WYSIWYG styles)
+
+## Setup
+
+At the top of the file, fill in the below fields in order for the script to connect to your
+Jira instance and create the tickets in the desired project.
+
+User Info:
+API_KEY - this can be generated from your user account settings, under the Security tab.
+JIRA_EMAIL - this should be the email address used for the user account tied to the api key above.
+
+Jira Instance Info:
+BASE_URL - atlassian url for your Jira instance.
+PROJECT_KEY - key of the project that you want the tickets created under. Found in your project's settings.
+
+## Usage
+
+The below flags can be used when running the script from the terminal.
+
+Required Flag:
+
+-t OR --topic
+Used for supplying the topic of the ticket that will be created. Any text that is
+wrapped in quotes after this flag will be added to the summary of the ticket.
+
+Optional Flags for changing ticket type:
+
+-s OR --story
+Set to true by default. Includes user statement, user types, and starter Acceptance Criteria list.
+
+-b OR --bug
+Overrides the story flag. Includes reproduction steps list, issue list, and expected list.
+
+-d OR --defect
+Overrides the story flag. Includes reproduction steps list, issue list, and expected list.
---
+++ jira-helper.py
@@ -0,0 +1,357 @@
+import argparse
+import json
+import requests
+
+# User Profile info from Jira
+API_KEY = ''
+JIRA_EMAIL = ''
+
+# Jira environment info
+BASE_URL = ''
+PROJECT_KEY = ''
+
+AUTH = requests.auth.HTTPBasicAuth(JIRA_EMAIL, API_KEY)
+HEADERS = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    prog='Jira Helper',
+    description='When configured correctly, this script assists in writing boilerplate text for Jira tickets and creates them in the backlog.'
+)
+
+parser.add_argument(
+    "-s", "--story", action="store_true", default=True, required=False,
+    help="Used to create a Story ticket in Jira. Defaults to true if omitted."
+)
+
+parser.add_argument(
+    "-b", "--bug", action="store_true", default=False, required=False,
+    help="Used to create a Bug ticket in Jira. Defaults to false if omitted."
+)
+
+parser.add_argument(
+    "-d", "--defect", action="store_true", default=False, required=False,
+    help="Used to create a Defect ticket in Jira. Defaults to false if omitted."
+)
+
+parser.add_argument(
+    "-t", "--topic", required=True,
+    help="Include a topic for your ticket wrapped in quotes. Example: -t 'My topic'."
+)
+
+args = parser.parse_args()
+
+
+def main():
+    topic = args.topic
+    issue = 'Story'
+
+    if args.bug == True:
+        issue = 'Bug'
+    elif args.defect == True:
+        issue = 'Defect'
+
+    # build description text & create ticket in Jira
+    desc = build_desc(topic, issue)
+    res = create_ticket(
+        summary=f"Placeholder - {topic}", project=PROJECT_KEY, description=desc, issue_type=issue, priority='Medium'
+    )
+    # print error messages in terminal if request fails
+    if res.status_code == 400:
+        json_res = json.loads(res.text)
+        print(f"Error: {json_res['errors']}")
+    else:
+        json_res = json.loads(res.text)
+        ticket_link = json_res['self']
+        # Display link to ticket in terminal after creation
+        print(f"Link: {ticket_link}")
+
+
+# Create the description text as Atlassian Document Format (ADF) with WYSIWYG styles
+# Generated in Atlassian's ADF Builder: https://developer.atlassian.com/cloud/jira/platform/apis/document/playground/
+def build_desc(topic, issue):
+    if issue == 'Story':
+        description = {
+            "version": 1,
+            "type": "doc",
+            "content": [
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": f"As a User, I need . . . {topic} . . ."
+                        }
+                    ]
+                },
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Note: "
+                        },
+                        {
+                            "type": "text",
+                            "text": "User in AC is District Admin, Building Admin, Staff User, District Volunteer Coordinator, Building Volunteer Coordinator, and Internal Super User.",
+                            "marks": [
+                                {
+                                    "type": "em"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Acceptance Criteria:",
+                            "marks": [
+                                {
+                                    "type": "strong"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "orderedList",
+                    "attrs": {
+                        "order": 1
+                    },
+                    "content": [
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": "User will navigate to . . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": "then . . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": "and then . . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    elif (issue == 'Bug') or (issue == 'Defect'):
+        description = {
+            "version": 1,
+            "type": "doc",
+            "content": [
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Steps to Reproduce:",
+                            "marks": [
+                                {
+                                    "type": "strong"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "orderedList",
+                    "attrs": {
+                        "order": 1
+                    },
+                    "content": [
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": "Navigate to . . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": "then . . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": "and then . . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Current Functionality:",
+                            "marks": [
+                                {
+                                    "type": "strong"
+                                },
+                                {
+                                    "type": "textColor",
+                                    "attrs": {
+                                            "color": "#ff5630"
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "orderedList",
+                    "attrs": {
+                        "order": 1
+                    },
+                    "content": [
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": ". . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "paragraph",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Expected Functionality:",
+                            "marks": [
+                                {
+                                    "type": "strong"
+                                },
+                                {
+                                    "type": "textColor",
+                                    "attrs": {
+                                        "color": "#36b37e"
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "type": "orderedList",
+                    "attrs": {
+                        "order": 1
+                    },
+                    "content": [
+                        {
+                            "type": "listItem",
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "type": "text",
+                                            "text": ". . ."
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+
+    return description
+
+
+def create_ticket(summary, project, description, issue_type, priority):
+    # Build Ticket
+    issue = {'fields': {}}
+    issue['fields']['summary'] = summary
+    issue['fields']['issuetype'] = {'name': issue_type}
+    issue['fields']['project'] = {'key': project}
+    issue['fields']['description'] = description
+    issue['fields']['priority'] = {'name': priority}
+
+    # Create ticket in Jira backlog
+    url = f"{BASE_URL}/rest/api/3/issue"
+    payload = json.dumps(issue)
+    response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=HEADERS,
+        auth=AUTH
+    )
+    return response
+
+
+if __name__ == "__main__":
+    main()
